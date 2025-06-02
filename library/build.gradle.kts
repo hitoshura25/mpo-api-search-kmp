2cdf1e8555ccd560f2c0176eb4760c0c96ff5aa6
Vinayak Menon <hitoshura25@users.noreply.github.com>
2025-06-02 13:29:03 -0500

    Updating deploy pipeline (#7)
    * Trying this again
    
    * Updating scope for package
    
    * Updating publish/deploy pipeline again

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "com.vmenon.mpo.user.api"
-version = "1.0.0"
+version = property("version")!!
 
 kotlin {
     jvm()
@@ -27,7 +27,7 @@ kotlin {
         }
     }
     js(IR) {
-        moduleName = "mpo-api-search-kmp"
+        moduleName = "@hitoshura25/mpo-api-search-kmp"
         browser {
             useCommonJs()
             commonWebpackConfig {
@@ -39,7 +39,7 @@ kotlin {
     }
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
-        moduleName = "mpo-api-search-kmp"
+        moduleName = "@hitoshura25/mpo-api-search-kmp"
         browser {
             useCommonJs()
             commonWebpackConfig {
