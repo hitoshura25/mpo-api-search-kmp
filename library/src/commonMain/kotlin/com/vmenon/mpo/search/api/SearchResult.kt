9c6572853d3374753352f490c227e00d7ad31eab
Vinayak Menon <hitoshura25@users.noreply.github.com>
2025-06-03 16:56:00 -0500

    Adding js bindings to kotlin use case (#16)

--- library/src/commonMain/kotlin/com/vmenon/mpo/search/api/SearchResult.kt
+++ library/src/commonMain/kotlin/com/vmenon/mpo/search/api/SearchResult.kt
@@ -1,5 +1,10 @@
 package com.vmenon.mpo.search.api
 
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
 data class SearchResult(
     val name: String,
     val artworkUrl: String?,
